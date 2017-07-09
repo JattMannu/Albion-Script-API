28748c6f8930b3f8bbdb35ba8f0b836c256d0c02
demmonic <evsubiw@gmail.com>
2017-07-08 23:27:19 -0400

    assemblyinfo

--- Albion Script API/Properties/AssemblyInfo.cs
+++ Albion Script API/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Ennui LLC")]
 [assembly: AssemblyProduct("AO")]
-[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyCopyright("Copyright © Ennui LLC 2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
