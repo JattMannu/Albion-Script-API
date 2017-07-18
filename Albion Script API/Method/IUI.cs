66999c9bedd6227070dbacf3f518980abf00997d
demmonic <evsubiw@gmail.com>
2017-07-18 14:39:44 -0400

    IUI docs

--- Albion Script API/Method/IUI.cs
+++ Albion Script API/Method/IUI.cs
@@ -7,7 +7,15 @@ namespace Ennui.Api.Method
 {
     public interface IUI : IApiModule
     {
+        /// <summary>
+        /// Shows a message on the user's game screen in a popup window.
+        /// </summary>
+        /// <param name="msg">The message to show</param>
         void ShowMessageOk(string msg);
+
+        /// <summary>
+        /// Whether or not UI input is enabled.
+        /// </summary>
         bool InputEnabled { set; }
     }
 }
