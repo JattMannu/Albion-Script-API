96dad9621dd5d3fc8d4c14b86445f0ff6d3ddfed
demmonic <evsubiw@gmail.com>
2017-07-20 05:24:27 -0400

    new api implementations

--- Albion Script API/MutableApi.cs
+++ Albion Script API/MutableApi.cs
@@ -26,6 +26,7 @@ namespace Ennui.Api
         public IFastTravelWindow FastTravelWindow { get; set; }
         public IFiles Files { get; set; }
         public IGame Game { get; set; }
+        public IGoldMarketWindow GoldMarketWindow { get; set; }
         public IGraphs Graphs { get; set; }
         public IHotkeys Hotkeys { get; set; }
         public IInput Input { get; set; }
