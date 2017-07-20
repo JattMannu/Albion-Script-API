6c80b483017ab2fc5e3f050b92a845086ddf461d
demmonic <evsubiw@gmail.com>
2017-07-20 04:33:14 -0400

    cleaning, new api stuff

---
+++ Albion Script API/Direct/Object/ISpellEffectAreaObject.cs
@@ -0,0 +1,13 @@
+﻿using Ennui.Api.Direct.Xml;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Ennui.Api.Direct.Object
+{
+    public interface ISpellEffectAreaObject : ISimulationObject
+    {
+        IXmlSpellConfig GetSpellXmlConfig();
+    }
+}
