c6b98b597fcc4b1996cea9e3e7b7fc1248fc80da
demmonic <evsubiw@gmail.com>
2017-07-22 04:34:04 -0400

    fucking docs

--- Albion Script API/Meta/HarvestableSetupState.cs
+++ Albion Script API/Meta/HarvestableSetupState.cs
@@ -7,7 +7,7 @@ namespace Ennui.Api.Meta
 {
     /// <summary>
     /// Whenever the local player tries to harvest a resource, it does a handful of checks, and returns
-    /// a numerical value depending on the state. 0 is success, everything else is a failure of some sort (e.g. someone else killed the mob.)
+    /// a numerical value depending on the state. Invalid means a failure of some sort (e.g. player doesn't own mob.)
     /// </summary>
     public enum HarvestableSetupState
     {
