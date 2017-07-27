d321e6514986cc455e70031a9f6a3e13fa727bb6
demmonic <evsubiw@gmail.com>
2017-07-27 02:56:02 -0400

    missing window feature

--- Albion Script API/Method/IExpeditionWindow.cs
+++ Albion Script API/Method/IExpeditionWindow.cs
@@ -9,6 +9,11 @@ namespace Ennui.Api.Method
     public interface IExpeditionWindow
     {
         /// <summary>
+        /// If the expedition window is open in game.
+        /// </summary>
+        bool IsOpen { get; }
+
+        /// <summary>
         /// The configs of all expeditions provided by this agent.
         /// </summary>
         List<IXmlExpedition> ExpeditionConfigs { get; }
