e969330831fef55ef291075609d2c519ed3a90c5
demmonic <evsubiw@gmail.com>
2017-07-29 16:50:11 -0400

    ok for real this time

--- Albion Script API/Method/ILoginErrorWindow.cs
+++ Albion Script API/Method/ILoginErrorWindow.cs
@@ -5,8 +5,25 @@ using System.Text;
 
 namespace Ennui.Api.Method
 {
+    /// <summary>
+    /// Provides a set of utilities for interacting with the interface that's shown
+    /// when a failed login happens.
+    /// </summary>
     public interface ILoginErrorWindow : IApiModule
     {
+        /// <summary>
+        /// If this window is open.
+        /// </summary>
+        bool IsOpen { get; }
+
+        /// <summary>
+        /// The error message being shown.
+        /// </summary>
+        string Message { get; }
+
+        /// <summary>
+        /// Attempts to click the ok button, causing the screen to close.
+        /// </summary>
         void ClickOk();
     }
 }
