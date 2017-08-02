08e9352c3e15df510c6afa45e34f2ffbe8aaa235
demmonic <evsubiw@gmail.com>
2017-08-02 14:00:22 -0400

    floating point directions

--- Albion Script API/GeneralUtils.cs
+++ Albion Script API/GeneralUtils.cs
@@ -132,5 +132,39 @@ namespace Ennui.Api
             }
             return Direction.None;
         }
+
+        /// <summary>
+        /// Calculates the direction between the first coordinates, and the second ones.
+        /// </summary>
+        /// <returns>The <see cref="GeneralUtils"/> between the two points.</returns>
+        /// <param name="x0">The x coordinate of the first point.</param>
+        /// <param name="y0">The y coordinate of the first point.</param>
+        /// <param name="x1">The x coordinate of the second point.</param>
+        /// <param name="y1">The y coordinate of the second point.</param>
+        public static Direction DirectionBetween(float x0, float y0, float x1, float y1)
+        {
+            var xDiff = x1 - x0;
+            var yDiff = y1 - y0;
+            if (xDiff.NearlyEquals(0.0f))
+            {
+                if (yDiff == 0) return Direction.None;
+                else if (yDiff > 0) return Direction.North;
+                else if (yDiff < 0) return Direction.South;
+            }
+            else if (yDiff.NearlyEquals(0.0f))
+            {
+                if (xDiff.NearlyEquals(0.0f)) return Direction.None;
+                else if (xDiff > 0) return Direction.East;
+                else if (xDiff < 0) return Direction.West;
+            }
+            else
+            {
+                if (xDiff > 0 && yDiff > 0) return Direction.NorthEast;
+                else if (xDiff > 0 && yDiff < 0) return Direction.SouthEast;
+                else if (xDiff < 0 && yDiff > 0) return Direction.NorthWest;
+                else if (xDiff < 0 && yDiff < 0) return Direction.SouthWest;
+            }
+            return Direction.None;
+        }
     }
 }
