df7d26d8c0785cf2fc28114ce291eff4b1bfb4a3
demmonic <evsubiw@gmail.com>
2017-08-29 04:20:08 -0400

    protected

--- Albion Script API/Object/ISilverObject.cs
+++ Albion Script API/Object/ISilverObject.cs
@@ -5,5 +5,9 @@
     /// </summary>
     public interface ISilverObject : IWorldObject
     {
+        /// <summary>
+        /// If this silver is protected (i.e. we can't take it)
+        /// </summary>
+        bool Protected { get; }
     }
 }
