210431b3c4d0e9084d2f060bb0b8cf8ef569e402
demmonic <evsubiw@gmail.com>
2017-08-29 04:15:57 -0400

    small expedition example

---
+++ Albion Script API/Example/ExpeditionsExample.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Ennui.Api.Example
+{
+    public class ExpeditionsExample : ApiResource
+    {
+        public ExpeditionsExample(IApi api) : base(api)
+        {
+        }
+
+        public void EnterExpedition(int tier)
+        {
+            if (ExpeditionWindow.IsOpen)
+            {
+                var target = ExpeditionWindow.ExpeditionConfigs.Find(e => (e.Tier == tier));
+                if (target == null)
+                {
+                    throw new InvalidOperationException("No expedition found with tier!");
+                }
+                ExpeditionWindow.Enter(target);
+            }
+        }
+    }
+}
