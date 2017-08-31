0ee6d5b093e38f164070afb69c33aa632e3f053a
demmonic <evsubiw@gmail.com>
2017-08-30 22:40:52 -0400

    assessment

--- Albion Script API/Xml/IXmlSpellConfig.cs
+++ Albion Script API/Xml/IXmlSpellConfig.cs
@@ -4,28 +4,28 @@ namespace Ennui.Api.Xml
     public interface IXmlSpellConfig : IXmlUiSpellConfig
     {
         /// <summary>
-        /// Retrieves the type that this spell can target.
+        /// The type that this spell can target.
         /// </summary>
-        /// <value>The type that this spell can target.</value>
         SpellTarget Target { get; }
 
         /// <summary>
-        /// Retrieves the category that the spell is in.
+        /// The category that the spell is in.
         /// </summary>
-        /// <value>The category that the spell is in.</value>
         SpellCategory Category { get; }
 
         /// <summary>
-        /// Retrieves the maximum amount of times that this spell can be stacked.
+        /// The maximum amount of times that this spell can be stacked.
         /// </summary>
-        /// <value>The maximum amount of times that this spell can be stacked.</value>
         int MaxStack { get; }
 
         /// <summary>
-        /// Retrieves the amount of energy required to cost this spell.
+        /// The amount of energy required to cast this spell.
         /// </summary>
-        /// <value>The amount of energy required to cast this spell.</value>
         int EnergyCost { get; }
 
+        /// <summary>
+        /// The assessment of the spell.
+        /// </summary>
+        SpellAssessment Assessment { get; }
     }
 }
