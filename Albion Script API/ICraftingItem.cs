08358f793cbbf1b6986eaad70a4793d5c589cca1
demmonic <evsubiw@gmail.com>
2017-09-07 09:34:11 -0400

    crafting window impl

---
+++ Albion Script API/ICraftingItem.cs
@@ -0,0 +1,13 @@
+﻿using Ennui.Api.Xml;
+
+namespace Ennui.Api
+{
+    public interface ICraftingItem
+    {
+        IItemConfig ItemConfig { get; }
+
+        string UniqueName { get; }
+
+        int StackSize { get; }
+    }
+}
--- Albion Script API/Method/ICraftingWindow.cs
+++ Albion Script API/Method/ICraftingWindow.cs
@@ -1,6 +1,11 @@
-﻿namespace Ennui.Api.Method
+﻿using System.Collections.Generic;
+
+namespace Ennui.Api.Method
 {
-    public interface ICraftingWindow : IApiModule
+    public interface ICraftingWindow : IApiModule, IWidget
     {
+        List<ICraftingItem> Selections { get; }
+
+        bool Craft(ICraftingItem item);
     }
 }
