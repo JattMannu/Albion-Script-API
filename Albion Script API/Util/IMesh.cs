15b816e7217363821aa726247b18e2e6c81edf0d
demmonic <evsubiw@gmail.com>
2017-11-30 10:35:41 -0500

    mesh docs

--- Albion Script API/Util/IMesh.cs
+++ Albion Script API/Util/IMesh.cs
@@ -2,10 +2,19 @@
 
 namespace Ennui.Api.Util
 {
+    /// <summary>
+    /// Represents a mesh.
+    /// </summary>
     public interface IMesh
     {
+        /// <summary>
+        /// The vertices of the mesh.
+        /// </summary>
         List<Vector3<float>> Vertices { get; }
 
+        /// <summary>
+        /// The triangle (vertex pointer) of the mesh.
+        /// </summary>
         List<int> Triangles { get; }
     }
 }
