8277dd699ae9a82b913a6b4604bd60f924e144b6
demmonic <evsubiw@gmail.com>
2017-11-30 10:35:09 -0500

    MathUtils docs

--- Albion Script API/Util/MathUtils.cs
+++ Albion Script API/Util/MathUtils.cs
@@ -1,12 +1,23 @@
 ﻿namespace Ennui.Api.Util
 {
+    /// <summary>
+    /// Provides a set of utilities relating to math.
+    /// </summary>
     public static class MathUtils
     {
-        public static float Sign(Vector2<float> p1, Vector2<float> p2, Vector2<float> p3)
+        private static float Sign(Vector2<float> p1, Vector2<float> p2, Vector2<float> p3)
         {
             return (p1.X - p3.X) * (p2.Y - p3.Y) - (p2.X - p3.X) * (p1.Y - p3.Y);
         }
 
+        /// <summary>
+        /// Determines if a coordinate it within a 2d triangle.
+        /// </summary>
+        /// <param name="pt">The point to check.</param>
+        /// <param name="v1">The first point of the triangle.</param>
+        /// <param name="v2">The second point of the triangle.</param>
+        /// <param name="v3">The third point of the triangle.</param>
+        /// <returns>If the provided point is within the triangle bounds.</returns>
         public static bool PointInTriangle(Vector2<float> pt, Vector2<float> v1, Vector2<float> v2, Vector2<float> v3)
         {
             var b1 = Sign(pt, v1, v2) < 0.0f;
