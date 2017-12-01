f1455a378896c0c961e3c5d6b0db024cbfd758ea
demmonic <evsubiw@gmail.com>
2017-11-30 21:03:38 -0500

    disposablewindow impl

---
+++ Albion Script API/Method/IDisposableWidget.cs
@@ -0,0 +1,13 @@
+﻿namespace Ennui.Api.Method
+{
+    /// <summary>
+    /// A widget that can be disposed of.
+    /// </summary>
+    public interface IDisposableWidget
+    {
+        /// <summary>
+        /// Disposes of the widget (i.e. closes it.)
+        /// </summary>
+        void Dispose();
+    }
+}
